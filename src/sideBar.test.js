b127afc6725404ba4d16bc625b34eddf7a6f2b6b
Robert Wiggins <wigginsro11@gmail.com>
2019-05-21 20:26:43 -0400

    sidebar test logic

--- src/sideBar.test.js
+++ src/sideBar.test.js
@@ -8,7 +8,7 @@ import Sidebar from './Sidebar';
 describe('<Sidebar />', () => {
    it('renders without crashing', () => {
        // Render the component, as JSON
-       const tree = renderer.create(<Sidebar />).toJSON();
+       const tree = renderer.create(<Sidebar id="1" name="Rob" avatar="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjEo9OK3K3iAhUKc98KHWC7AJ8QjRx6BAgBEAU&url=https%3A%2F%2Fedecorati.com%2Fw%2Fflower-images-hd.html&psig=AOvVaw3BggLjgxan2CvnqEIKKMsi&ust=1558565731019423" inSession="true" onStage="false" />).toJSON();
        // Check whether it matches the previous snapshot
        // Stored in __snapshots__/App.test.js.snap
        expect(tree).toMatchSnapshot();
